ba3a539f27f58959d29c7d14ac37f40d7bb1321c
Prelang Builder <builder@prelang.com>
2014-02-05 21:39:07 +0000

    Set up member route for #vote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 RankIt::Application.routes.draw do
-  resources :ideas
+  resources :ideas do
+    #->Prelang (voting/acts_as_votable)
+    member do
+      get "vote"
+    end
+  end
+
 
   resources :industries
 
