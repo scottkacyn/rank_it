ec4099af174c7368f98cd9ed2479a3874743e1ea
Prelang Builder <builder@prelang.com>
2014-02-05 21:39:08 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,7 +3,7 @@ class IdeasController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_idea, only: [:show, :edit, :update, :destroy]
+  before_action :set_idea, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /ideas
   # GET /ideas.json
