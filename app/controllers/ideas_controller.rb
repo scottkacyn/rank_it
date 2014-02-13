ae6777a54b62f10fe51a3cfdcf2b7c38f3678432
Scott Lieberman <scottklieberman@gmail.com>
2014-02-13 15:10:54 -0800

    Made logged out view better

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,7 @@
 class IdeasController < ApplicationController
 
   #->Prelang (scaffolding:rails/scope_to_user)
-  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
-
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy, :vote, :unvote, :comment]
   before_action :set_idea, only: [:show, :edit, :update, :destroy, :vote, :unvote, :comment]
 
   # GET /ideas
